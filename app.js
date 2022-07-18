8ddde4f1f80b8a4df4129839bfe9d53d80f4ee25
mars1448 <lekeeseschool@gmail.com>
2022-07-18 13:09:26 -0700

    Updated fuctionality of project

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
-const button = document.querySelectorAll('button')
+const button = document.querySelectorAll("button");
+const playerBox = document.getElementById("player-box");
+const cpuBox = document.getElementById("computer-box");
+let results = document.getElementById("results-box");
+let plScore = 0;
+let cpuScore = 0;
+
 button.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    let playerChoice = e.target.value
-  })
-})
-const choices = ['Rock', 'Paper', 'Scissors']
+  btn.addEventListener("click", (e) => {
+    let playerChoice = e.target.value;
+    round(playerChoice, getComputerChoice());
+    win(plScore, cpuScore);
+  });
+});
+const choices = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
-  return choices[Math.floor(Math.random() * choices.length)]
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function win() {
-  return computerScore > playerScore ? 'The Computer Wins' : 'The Player Wins'
+function win(playerScore, computerScore) {
+  if (playerScore === 5 || computerScore === 5) {
+    return computerScore > playerScore
+      ? (results.textContent = "The Computer Wins")
+      : (results.textContent = "The Player Wins");
+  }
 }
-
 function round(player, computer) {
-  
+  if (plScore === 5 || cpuScore === 5) return;
+  switch (true) {
+    case player === computer:
+      results.textContent = "hmmm it's a tie";
+      break;
+    case player == "Rock" && computer === "Scissors":
+    case player === "Paper" && computer === "Rock":
+    case player === "Scissors" && computer === "Paper":
+      plScore++;
+      playerBox.textContent = plScore;
+      results.textContent = `Player wins`;
+      break;
+    case player == "Rock" && computer === "Paper":
+    case player === "Paper" && computer === "Scissors":
+    case player === "Scissors" && computer === "Rock":
+      cpuScore++;
+      cpuBox.textContent = cpuScore;
+      results.textContent = `Computer wins`;
+      break;
+  }
 }
-// round()
