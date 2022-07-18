1f0850422fb7313c8bb8d9e65f35fa569c44f823
mars1448 <lekeeseschool@gmail.com>
2022-07-18 11:29:39 -0700

    Updated UI

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-
+const button = document.querySelectorAll('button')
+button.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    let playerChoice = e.target.value
+  })
+})
 const choices = ['Rock', 'Paper', 'Scissors']
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)]
 }
 
-function round(player, computer) {
-  computer = getComputerChoice();
-  const answer = prompt('Rock Paper or Scissors')
-  player = answer[0].toUpperCase() + answer.slice(1)
-  console.log(`The player's choice: '${player}' vs The computer's choice: '${computer}'`)
-  if(player === 'Rock') {
-    if(computer === 'Paper') console.log('Computer Wins')
-    else if (computer === 'Scissors') console.log('Player Wins')
-  }
-  if(player === 'Paper') {
-    if(computer === 'Scissors') console.log('Computer Wins')
-    else if (computer === 'Rock') console.log('Player Wins')
-  }
-  if(player === 'Scissors') {
-    if(computer === 'Rock') console.log('Computer Wins')
-    else if (computer === 'Paper') console.log('Player Wins')
-  } else if (player === computer) console.log('tie')
+function win() {
+  return computerScore > playerScore ? 'The Computer Wins' : 'The Player Wins'
 }
-round()
+
+function round(player, computer) {
+  
+}
+// round()
